91a119aa87d5f5603bc63f27c1359226484f3b0f
LeoGomes0 <leonardobrgomes@outlook.com>
2024-02-01 11:42:47 -0300

    fix(package.json): correção de bugs do nodemon
    não estava conseguindo realizar a atualização em tempo real, porém, agora está funcionando.
    
    BREAKING CHANGE: atualização em tempo real do projeto

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response, NextFunction } from "express";
+import usersRoute from "./routes/usersRoute";
 
 const app = express();
 
-app.get("/status", (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: "bebida" });
-});
+
+app.use(usersRoute);
+
 
 app.listen(3000, () => {
     console.log("Server Online!");
